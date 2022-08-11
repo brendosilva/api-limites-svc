c79a0d19311b69cbaff96d5641392b75b27b6fb7
Brendo Silva <breendo.01@gmail.com>
2022-08-11 17:56:24 -0300

    flyway database migrations

---
+++ src/main/resources/db/migration/V3__INSERT_LIMITE.sql
@@ -0,0 +1,18 @@
+INSERT INTO limite_diario (agencia, conta, data, valor ) VALUES (
+    223,
+    8899663,
+    now(),
+    988
+);
+INSERT INTO limite_diario (agencia, conta, data, valor ) VALUES (
+    223,
+    8899665,
+    now(),
+    310
+);
+INSERT INTO limite_diario (agencia, conta, data, valor ) VALUES (
+    223,
+    8899665,
+    now(),
+    510
+);
