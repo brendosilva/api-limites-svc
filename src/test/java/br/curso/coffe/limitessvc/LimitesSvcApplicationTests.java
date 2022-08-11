175187a37507f44e6e03fc629bbf9c3169c05fb7
Brendo Silva <breendo.01@gmail.com>
2022-08-11 13:55:38 -0300

    configuracoes inicial, docker e properties

---
+++ src/test/java/br/curso/coffe/limitessvc/LimitesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.curso.coffe.limitessvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
